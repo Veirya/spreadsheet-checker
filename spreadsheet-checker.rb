df522af3f760e134ead999057a7dde7cdec01972
Veirya <47880560+Veirya@users.noreply.github.com>
2020-03-17 16:22:25 -0700

    Add files via upload
    Added support for Rank Changes and checking for promotion candidates based on time. Tidied up code and added header.

--- spreadsheet-checker.rb
+++ spreadsheet-checker.rb
@@ -1,21 +1,44 @@
+=begin
+  Spreadsheet Checker, by Giovanni "Veirya" Oliver (2020)
+  :::::::::::::::::::::::::::::::::::::::::::::::::
+  Simple script that looks at a Google Sheets sheet of FFXIV FC member data,
+    then compares it to information from the Lodestone. Information considered
+    to be of interest is then printed out for the user to see. Current iteration
+    only works for the FC "FullMetal Alliance" and a hard-coded sheet.
+  Information considered of interest by the script:
+    - New/Old Members
+    - Name Changes
+    - Rank Changes
+    - Possibile Promotion Candidates (FMA New Recruits only)
+    - Lack of Date for a Member in Spreadsheet.
+=end
+
+# Dependencies
+#####################
 require 'net/http'
 require 'json'
 require 'ostruct'
+require 'date'
 require "google/apis/sheets_v4"
 require "googleauth"
 require "googleauth/stores/file_token_store"
 require "fileutils"
 
+# Constants
+#####################
 OOB_URI = "urn:ietf:wg:oauth:2.0:oob".freeze
-APPLICATION_NAME = "Google Sheets API Ruby Quickstart".freeze
+APPLICATION_NAME = "FC Spreadsheet Checker".freeze
 CREDENTIALS_PATH = "credentials.json".freeze
 # The file token.yaml stores the user's access and refresh tokens, and is
 # created automatically when the authorization flow completes for the first
 # time.
 TOKEN_PATH = "token.yaml".freeze
 SCOPE = Google::Apis::SheetsV4::AUTH_SPREADSHEETS_READONLY
+TODAY = DateTime.parse(Time.now.strftime("%y-%m-%d"))
 
-print "Authenticating and Initializing Google API... "
+# Methods
+#####################
+
 ##
 # Ensure valid credentials, either by restoring from the saved credentials
 # files or intitiating an OAuth2 authorization. If authorization is required,
@@ -40,78 +63,175 @@ def authorize
   credentials
 end
 
-# Initialize the API
+##
+# Reads and formats member data from a Google Sheets spreadsheet.
+#
+# [Google::Apis::SheetsV4::SheetsService] service: Google Sheets API service
+# @return [Hash[Integer : OpenStruct]] Formatted member data from sheet 
+def readSheet(service)
+  print "Retrieving FC members from spreadsheet... "
+  # Retrieve Current FC members data from spreadsheet
+  spreadsheet_id = "1i09Ey3KFzvJENkToV1o5bSh89AML85fW6cz0o0IMgI0"
+  range = "Members (Condensed)!A2:I"
+  response = service.get_spreadsheet_values spreadsheet_id, range
+
+  sheetMembers = Hash.new
+  response.values.each do |row|
+    data = OpenStruct.new
+    data.Name = row[0]
+    data.Rank = row[2]
+
+    # Dates should be in format of MM/DD/YYYY. Empty entries will be flagged.
+    data.Date = row[5]
+    if data.Date == ""
+      data.Date = row[4]
+    end
+    if data.Date != ""
+      data.Date = data.Date.split("/")
+      data.Date = DateTime.parse("#{data.Date[2]}-#{data.Date[0]}-#{data.Date[1]}")
+    end
+
+    data.ID = row[8].to_i
+    sheetMembers[data.ID] = data
+  end
+  puts "Finished."
+
+  return sheetMembers
+end
+
+##
+# Retrieves and formats member data from the FFXIV Lodestone.
+#
+# @return [Hash[Integer : OpenStruct]] Formatted member data from Lodestone
+def checkLode()
+  print "Retrieving FC members from xivapi... "
+  # Retrieve current FC members from Lodestone and convert to hash
+  # mapped by ID
+  url = "https://xivapi.com/freecompany/9232097761132854687?data=FCM"
+  uri = URI(url)
+  response = Net::HTTP.get(uri)
+  parse = JSON.parse(response, object_class: OpenStruct).FreeCompanyMembers
+  lodeMembers = Hash.new
+
+  parse.each do |member|
+  	lodeMembers[member.ID] = member
+  end
+  puts "Finished."
+  return lodeMembers
+end
+
+##
+# Compare member data from sheet and Lodestone, flagging information considered to be
+# of interest to the user. May remove entries from sheetMembers. Does not modify
+# lodeMembers. Fills requested parameters with their respective flagged information.
+#
+# [Hash[Integer : OpenStruct]] sheetMembers : Member data from a Google Sheets spreadsheet
+# [Hash[Integer : OpenStruct]] lodeMembers : Member data from the FFXIV Lodestone
+# [Array] <<An empty Array for each type of information of interest>>
+# @return nil
+def processData(sheetMembers, lodeMembers, newMems, nameChanges, rankChanges, noDates, needPromos)
+  lodeMembers.each do |key, member|
+  	if sheetMembers[key]
+  		if sheetMembers[key].Name != member.Name
+  			nameChanges.push("'#{sheetMembers[key].Name}' changed their name to '#{member.Name}'.")
+  		end
+
+      if sheetMembers[key].Rank != member.Rank
+        rankChanges.push("#{sheetMembers[key].Name} has changed ranks from ".concat(
+          "'#{sheetMembers[key].Rank}' to '#{member.Rank}'."))
+      end
+
+      if sheetMembers[key].Date.is_a? String
+        noDates.push("#{member.Name}")
+      elsif member.Rank == "New Recruit"
+        diff = (TODAY - sheetMembers[key].Date).to_i
+        if diff >= 30
+          needPromos.push("#{member.Rank} #{member.Name} may need a promotion. (#{diff} days since joined)")
+        end
+      end
+
+  		sheetMembers.delete(key)
+  	else
+      # New members.
+  		newMems.push("#{member.Name}\tID: #{key}")
+  	end
+  end
+  return nil
+end
+
+##
+# Prints the data from the requested parameters in a user-readable format.
+#
+# [Hash[Integer : OpenStruct]] sheetMembers : Processed member data
+# [Array] <<Arrays of strings containing information of interest>>
+# @return nil
+def printData(sheetMembers, newMems, nameChanges, rankChanges, noDates, needPromos)
+  puts "New Members"
+  puts ":::::::::::::::::::::::"
+  newMems.each do |string|
+  	puts string
+  end
+  puts ""
+
+  puts "No longer in FC"
+  puts ":::::::::::::::::::::::"
+  sheetMembers.each do |key, member|
+  	puts "#{member.Name}\tID: #{key}"
+  end
+  puts ""
+
+  puts "Name Changes"
+  puts ":::::::::::::::::::::::"
+  nameChanges.each do |string|
+  	puts string
+  end
+  puts ""
+
+  puts "Rank Changes"
+  puts ":::::::::::::::::::::::"
+  rankChanges.each do |string|
+    puts string
+  end
+  puts ""
+
+  puts "Possibile Promotion Candidates"
+  puts ":::::::::::::::::::::::"
+  needPromos.each do |string|
+    puts string
+  end
+  puts ""
+
+  puts "In Need of Dates"
+  puts ":::::::::::::::::::::::"
+  noDates.each do |string|
+    puts string
+  end
+  puts ""
+  return nil
+end
+
+# Main Code Body
+#####################
+puts "Today's date is #{TODAY.mon}/#{TODAY.day}/#{TODAY.year}."
+# Initialize the Google Sheets API
+print "Authenticating and Initializing Google API... "
 service = Google::Apis::SheetsV4::SheetsService.new
 service.client_options.application_name = APPLICATION_NAME
 service.authorization = authorize
 puts "Finished."
 
-print "Retrieving FC members from spreadsheet... "
-# Retrieve Current FC members from spreadsheet and convert to hash
-# mapped by ID
-spreadsheet_id = "1i09Ey3KFzvJENkToV1o5bSh89AML85fW6cz0o0IMgI0"
-range = "Members (Condensed)!A2:I"
-response = service.get_spreadsheet_values spreadsheet_id, range
-
-sheetMembers = Hash.new
-response.values.each do |row|
-  data = OpenStruct.new
-  data.Name = row[0]
-  data.Rank = row[2]
-  data.ID = row[8].to_i
-  sheetMembers[data.ID] = data
-end
-puts "Finished."
-
-print "Retrieving FC members from xivapi... "
-# Retrieve current FC members from Lodestone and convert to hash
-# mapped by ID
-url = "https://xivapi.com/freecompany/9232097761132854687?data=FCM"
-uri = URI(url)
-response = Net::HTTP.get(uri)
-parse = JSON.parse(response, object_class: OpenStruct).FreeCompanyMembers
-lodeMembers = Hash.new
-
-parse.each do |member|
-	member.ID = 
-	lodeMembers[member.ID] = member
-end
-puts "Finished."
+sheetMembers = readSheet(service)
+lodeMembers = checkLode()
 puts ""
 
-=begin
-		
-=end
-newMems = Hash.new
+newMems = Array.new
 nameChanges = Array.new
-lodeMembers.each do |key, member|
-	if sheetMembers[key]
-		if sheetMembers[key].Name != member.Name
-			nameChanges.push("'#{sheetMembers[key].Name}' changed their name to '#{member.Name}'.")
-		end
-		sheetMembers.delete(key)
-	else
-		newMems[key] = member
-	end
-end
+rankChanges = Array.new
+noDates = Array.new
+needPromos = Array.new
 
-puts "New Members:"
-newMems.each do |key, member|
-	puts "Name: #{member.Name}, ID: #{key}"
-end
-puts ""
-
-puts "No longer in FC:"
-sheetMembers.each do |key, member|
-	puts "Name: #{member.Name}, ID: #{key}"
-end
-puts ""
-
-puts "Name Changes:"
-nameChanges.each do |string|
-	puts string
-end
-puts ""
+processData(sheetMembers, lodeMembers, newMems, nameChanges, rankChanges, noDates, needPromos)
+printData(sheetMembers, newMems, nameChanges, rankChanges, noDates, needPromos)
 
 print "Press enter to quit."
 gets.chomp
